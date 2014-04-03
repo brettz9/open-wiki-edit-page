49d8511956a3e91ab4e63811f6fa2454cb06668e
Brett Zamir <brettz9@yahoo.com>
2014-04-03 13:28:12 +0800

    Initial commit (i18n, JSLint)

---
+++ data/openISBNNewTab.js
@@ -0,0 +1,17 @@
+// openISBNNewTab.js - Open Wiki Edit Page's module
+/*globals self*/
+(function () {'use strict';
+
+self.on('context', function (node) {
+  return (/\/wiki\/|[?]title=/).test(node.href) && decodeURIComponent(node.href).indexOf('Special:BookSources') > -1;
+});
+self.on('click', function (node, data) {
+  var href = node.href.replace(
+    /^.*?(?:(\/)wiki\/|[?]title=)Special:BookSources\/([^?&]*)/,
+    'http://www.amazon.com/gp/search/?ie=UTF8&Adv-Srch-Books-Submit.y=0&sort=relevanceexprank&search-alias=stripbooks&tag=wiki-addon-20'+
+      '&linkCode=ur2&unfiltered=1&camp=1789&Adv-Srch-Books-Submit.x=0&field-dateop=&creative=390957&field-isbn=$2'
+  );
+  self.postMessage(href);
+});
+
+}());
